9b7a290a73ed9ad103979d7f43f445bba31552dc
lenz90 <lenz.90@gmail.com>
2019-12-29 16:38:29 -0500

    Se agregan los cambios para el servidor eureka

---
+++ src/test/java/pe/com/producs/lenz/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.producs.lenz.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
